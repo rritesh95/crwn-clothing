705571e17a82dc4c27dbc066be7c9624e366224d
Riteshkumar Patel <ritepate@in.ibm.com>
2021-02-07 15:40:35 +0530

    updating routes in application

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
