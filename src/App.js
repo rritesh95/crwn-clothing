fe8ba630719969dd9cc8242f9a43691f8b5df565
Riteshkumar Patel <ritepate@in.ibm.com>
2021-04-10 16:31:26 +0530

    added signup functionality and craeted firestore database

--- src/App.js
+++ src/App.js
@@ -21,9 +21,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot( snapshot => {
+          this.setState({
+            currentUser : {
+              id : userRef.id,
+              ...snapshot.data()
+            }
+          });
+          
+          console.log(this.state);
+        });
+      }
+
+      this.setState({
+        currentUser : userAuth
+      });
+    });
   }
 
   componentWillUnmount(){
